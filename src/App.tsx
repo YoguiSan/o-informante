9ee0667c1363d96b0191681bf46b877ea6f84231
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2021-02-23 21:38:32 -0300

    Feitas algumas configurações iniciais

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any;
+  }
+}
+
+const App: React.FC<JSX.IntrinsicElements> = () => (
+  <main id="principal">
+    <h1>O informante</h1>
+    <h2>Você é livre. A informação também deve ser</h2>
+  </main>
+);
+
+export default App;
