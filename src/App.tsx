e6446a3f6a26f80ef84dee1128f868dc6e2e1663
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2021-03-01 14:59:20 -0300

    Feitas configurações básicas, projeto rodando

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 
+/*
 declare namespace JSX {
   interface IntrinsicElements {
     [elemName: string]: any;
   }
 }
+*/
 
-const App: React.FC<JSX.IntrinsicElements> = () => (
+// const App: React.FC<JSX.IntrinsicElements> = () => (
+const App = () => (
   <main id="principal">
     <h1>O informante</h1>
     <h2>Você é livre. A informação também deve ser</h2>
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import * as ReactDOM from 'react-dom';
-
-import App from './App.js';
-
-ReactDOM.render(App, document.getElementById('root'));
