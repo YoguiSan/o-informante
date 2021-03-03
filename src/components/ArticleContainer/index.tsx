3d6f0b5614da192af3667a88bad3fdee77bc1553
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2021-03-03 12:18:05 -0300

    Ajustado componente ArticleContainer

--- src/components/ArticleContainer/index.tsx
+++ src/components/ArticleContainer/index.tsx
@@ -7,10 +7,36 @@ type ArticleContainerProps = {
 }
 
 const ArticleContainer = ({ url }: ArticleContainerProps) => {
-  console.log(url)
+  /*
+  const [html, setHtml] = React.useState<any>({});
+
+  React.useEffect(() => {
+    const doc = new Promise((resolve, reject) => {
+      const response = open(url);
+
+      resolve(response);
+    });
+
+    console.log(doc)
+
+    setHtml(doc);
+  }, [url]);
+
+  const open = async (url: string): Promise<string|void> => {
+    const resp = await fetch(url, { mode: 'no-cors' })
+      .then((response) => response.text())
+      .then((html) => html)
+      .catch((error) => {
+        console.warn(error);
+      });
+
+    return resp;
+  }
+  */
+
   return(
-    <article id="open-article">
-      <iframe src={url}></iframe>
+    <article id="open-article-container">
+      {url && <object id="open-article-content" type="text/html" data={url} />}
     </article>
   );
 };
